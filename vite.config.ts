13273e2d44dd04086b41e803e8a6f6c47c831831
Sahiba Haroon <sahibaharoon@Sahibas-MacBook-Pro.local>
2025-08-01 08:24:41 +0530

    Allow render.com host in V

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  server: {
+    allowedHosts: ['billsplit-4mdm.onrender.com']
+  }
 });
